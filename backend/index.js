06a0d4a97dce8082ea2be8ab784a7b10f9350cf6
Brandon Walton <BrandonWalton101102@gmail.com>
2024-03-15 00:19:27 -0500

    Post User route
    + User can now be posted and stored in the database
    + User password is hashed

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,37 @@
-// Middleware Installation: backend/installations/MiddleWare-Installation.txt
+//NOTE: Middleware Installation: backend/installations/MiddleWare-Installation.txt
 
 const express = require('express');
 const cors = require('cors');
-const sqlite3 = require('sqlite3').verbose();
 const routes = require('./controller/routes')
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const PORT = 5500;
 
+// Setting Up Middleware
 app.use(express.json());
+app.use(cors());
 
-// Connecting to Server for HTTP Request
+// Establishing server via port: 5500
 app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`)
 });
 
+
 // Connecting to local database
-const path = "C:\\Users\\ender\\OneDrive\\Documents\\GitHub\\Sports-Application\\backend\\database\\sports-app.db"; // Change to path
-const db = new sqlite3.Database('path', (err) => {
+const path = "C:\\Users\\ender\\OneDrive\\Documents\\GitHub\\Sports-Application\\backend\\database\\sports-app.db"; // Change to local path
+const db = new sqlite3.Database(path, (err) => {
     if (err) {
         console.error('Error opening database:', err.message);
     } else {
-        console.log('Connected to the database.');
+        console.log('Connected to SQLite Database.');
     }
 })
 
-const userinfo = `CREATE TABLE userinfo ( userID INTEGER PRIMARY KEY, username TEXT(50), password TEXT(50))`
+//Exporting db instance to routes.js
+app.use((req, res, next) => {
+    req.db = db; // Attach db instance to req object
+    next();
+});
 
-
+// Setting up routes
+app.use('/api', routes);
