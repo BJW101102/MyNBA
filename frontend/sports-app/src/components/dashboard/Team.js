d81a6b09decd30cfd6e44baf55ebdd691f95ee16
Brandon Walton <BrandonWalton101102@gmail.com>
2024-03-21 13:04:32 -0500

    Dashboard update
    + Increased Stat's container size
    + Added Team name as the title

--- frontend/sports-app/src/components/dashboard/Team.js
+++ frontend/sports-app/src/components/dashboard/Team.js
@@ -10,6 +10,7 @@ function Team({logo}) {
     return (
         <div>
             <Card className="team m-2 p-2" style={{ border: "2px solid white" }} >
+            <Card.Title className="team-font-size" style={{ background: "#E3E1E1", border: "2px solid black" }}>Team Name</Card.Title>
                 <Card.Body className="d-flex flex-row" style={{ border: "2px solid white", overflowX: "auto" }}>
                     <Row>
                             {/* Logo Container */}
@@ -20,7 +21,7 @@ function Team({logo}) {
                                 </Card.Body>
                             </Col>
                             {/* Team Stats Container */}
-                            <Card.Body style={{ width: "30vh", border: "2px solid black" }}>
+                            <Card.Body style={{ width: "43vh", border: "2px solid black" }}>
                                 <Card.Title className="team-font-size" style={{ border: "2px solid black" }}>Stats</Card.Title>
                                 <div className="text-container" style={{ border: "2px solid black" }}> {/* Container for the text fields*/}
                                     <Card.Text className="team-font-size">Conference: </Card.Text>
