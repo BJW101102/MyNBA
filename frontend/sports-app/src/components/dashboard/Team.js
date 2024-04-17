032e0b5e37dac5c7eb342ad182f6c013d96a4ce7
shelbyantill <shelbyeantill@gmail.com>
2024-04-17 10:42:31 -0500

    making data visible

--- frontend/sports-app/src/components/dashboard/Team.js
+++ frontend/sports-app/src/components/dashboard/Team.js
@@ -1,51 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import Image from 'react-bootstrap/Image';
 
-function Team({logo}) {
-    //NOTE: I put all the styles inline since it may be easier to edit and see what is going on, feel free to export it to the CSS sheet. 
-    return (
-        <div>
-            <Card className="team m-2 p-2" style={{ border: "2px solid white" }} >
-            <Card.Title className="team-font-size" style={{ background: "#E3E1E1", border: "2px solid black" }}>Team Name</Card.Title>
-                <Card.Body className="d-flex flex-row" style={{ border: "2px solid white", overflowX: "auto" }}>
-                    <Row>
-                            {/* Logo Container */}
-                            <Col>
-                                <Card.Body style={{ width: "30vh", border: "2px solid black" }}>
-                                    <Card.Img fluid variant="top"  src={logo} rounded />
-                                    <Card.Text className="team-font-size"></Card.Text>
-                                </Card.Body>
-                            </Col>
-                            {/* Team Stats Container */}
-                            <Card.Body style={{ width: "43vh", border: "2px solid black" }}>
-                                <Card.Title className="team-font-size" style={{ border: "2px solid black" }}>Stats</Card.Title>
-                                <div className="text-container" style={{ border: "2px solid black" }}> {/* Container for the text fields*/}
-                                    <Card.Text className="team-font-size">Conference: </Card.Text>
-                                    <Card.Text className="team-font-size">Standing: </Card.Text>
-                                    <Card.Text className="team-font-size">W/L: </Card.Text>
-                                </div>
-                            </Card.Body>
-                    </Row>
-                </Card.Body>
-                <Row>
-                    <Col>
-                        <ListGroup className="team-font-size" style={{ maxHeight: '100px', overflowY: 'auto' }}>
-                            <ListGroup.Item>Player Name </ListGroup.Item>
-                            <ListGroup.Item>Player Name </ListGroup.Item>
-                            <ListGroup.Item>Player Name </ListGroup.Item>
-                            <ListGroup.Item>Player Name </ListGroup.Item>
-                            <ListGroup.Item>Player Name </ListGroup.Item>
-                        </ListGroup>
-                    </Col>
-                </Row>
-            </Card>
-        </div>
-    );
+function Team({ team }) {
+  return (
+    <div>
+      <Card className="team m-2 p-2" style={{ border: "2px solid white" }}>
+        <Card.Title className="team-font-size" style={{ background: "#E3E1E1", border: "2px solid black" }}>{team.name}</Card.Title>
+        <Card.Body className="d-flex flex-row" style={{ border: "2px solid white", overflowX: "auto" }}>
+          <Card.Img fluid variant="top" src={team.logo} rounded style={{ width: "30vh", border: "2px solid black" }} />
+        </Card.Body>
+        <ListGroup className="team-font-size" style={{ maxHeight: '100px', overflowY: 'auto' }}>
+          {team.players.map(player => (
+            <ListGroup.Item key={player.playerID}>{player.firstName} {player.lastName}</ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Card>
+    </div>
+  );
 }
 
 export default Team;
