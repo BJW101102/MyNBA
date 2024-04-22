7159d8a59bdaf978a539eaa809a6ca031ff00ec6
Brandon Walton <BrandonWalton101102@gmail.com>
2024-04-22 01:18:25 -0500

    Edited Card Layouts

--- frontend/sports-app/src/components/dashboard/Team.js
+++ frontend/sports-app/src/components/dashboard/Team.js
@@ -2,6 +2,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import CardBody from 'react-bootstrap/esm/CardBody';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
+
+
 
 function isDark(color) {
     const hex = color.replace("#", "");
@@ -24,18 +30,37 @@ function Team({ team }) {
         <div>
             <Card className="team m-2 p-0 custom-card" style={{ margin: "0", padding: "0" }}>
                 <Card.Title className="team-font-size" style={{
-                    backgroundColor: team.secondary, 
+                    backgroundColor: team.secondary,
                     color: textColor, // Only apply the dynamic text color to the title
-                    width: "100%", 
-                    padding: "10px 20px", 
-                    borderTopLeftRadius: "6px", 
+                    width: "100%",
+                    padding: "10px 20px",
+                    borderTopLeftRadius: "6px",
                     borderTopRightRadius: "6px"
                 }}>
-                    {team.code}, {team.name}
+                 {team.name}
                 </Card.Title>
                 <Card.Body className="d-flex flex-row" style={{ backgroundColor: "white", overflowX: "auto" }}>
-                    <Card.Img fluid variant="top" src={team.logo} style={{ height: "25vh", width: "40vh", marginRight: "5vh" }} />
-                    <ListGroup className="team-font-size" style={{ maxHeight: '25vh', overflowY: 'auto' }}>
+                    <Row>
+                        <Col className="d-flex justify-content-center align-items-center">
+                            <Card.Img fluid variant="top" src={team.logo} style={{ height: "25vh", width: "40vh" }}
+                            />
+                        </Col>
+                     
+                        <Col style={{width: "100vh"}}>
+                            <Card className="team m-2 p-0">
+                                <Card.Title className="team-font-size" style={{
+                                    backgroundColor: team.primary,
+                                    color: "white", // Only apply the dynamic text color to the title
+                                    width: "100%",
+                                    padding: "10px 20px",
+                                    borderTopLeftRadius: "6px",
+                                    borderTopRightRadius: "6px",
+
+                                }}>
+                                    {team.code}, Team Roster
+                                </Card.Title>
+                                <CardBody>
+                                    <ListGroup className="team-font-size" style={{ maxHeight: '15vh', overflowY: 'auto'}}>
                         {team.players.map(player => (
                             // Keep the player names' color black or any color that contrasts with their background
                             <ListGroup.Item key={player.playerID} style={{ color: '#000' }}>
@@ -43,9 +68,19 @@ function Team({ team }) {
                             </ListGroup.Item>
                         ))}
                     </ListGroup>
+                                </CardBody>
+
+                            </Card>
+                        </Col>
+
+                    </Row>
+
+
+
                 </Card.Body>
             </Card>
         </div>
+
     );
 }
 
